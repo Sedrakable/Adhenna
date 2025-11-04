5e678929c0f06ee90e074fa9b721c32ed6af9542
Sedrakable <sedrak005@gmail.com>
2025-11-04 17:16:25 -0500

    SEO stuff updated

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -8,10 +8,7 @@ interface SEOProps extends ISeo {
   path: string;
   crawl?: boolean;
 }
-// Dynamic import for the BreadcrumbJsonLd component
-const DynamicBreadcrumbJsonLd = dynamic(() =>
-  import("next-seo").then((mod) => mod.BreadcrumbJsonLd)
-);
+
 export const setMetadata = ({
   locale,
   metaTitle,
@@ -59,10 +56,11 @@ export const setMetadata = ({
       ],
     },
     alternates: {
-      canonical: `https://www.adhennatatoo.com/${locale}${path}`,
+      canonical: `https://www.adhennatattoo.com/${locale}${path}`,
       languages: {
-        en: `https://www.adhennatatoo.com/en${path}`,
-        fr: `https://www.adhennatatoo.com/fr${path}`,
+        en: `https://www.adhennatattoo.com/en${path}`,
+        fr: `https://www.adhennatattoo.com/fr${path}`,
+        "x-default": `https://www.adhennatattoo.com/fr${path}`,
       },
     },
   };
